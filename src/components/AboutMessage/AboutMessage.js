a009923e05c30b7056fedaeb88f45a964ca3837e
Raul <raulch1998@gmail.com>
2019-02-25 16:38:57 -0400

    Style about page

--- src/components/AboutMessage/AboutMessage.js
+++ src/components/AboutMessage/AboutMessage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const aboutMessage = props => (
-  <div>
+  <div className="about-message">
     <h1>About me</h1>
-    <p>Lorem ipsum khasjnasj</p>
+    <p>
+      Laborum et doloribus exercitationem numquam vero rerum. Velit rerum ullam
+      et repellat. Ducimus temporibus nesciunt eaque impedit explicabo ipsa.
+      Quod et in quasi et. Ducimus enim beatae omnis est. Officia magnam impedit
+      molestiae in impedit dolorem.
+    </p>
   </div>
 );
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,15 +3,26 @@ import { NavLink } from 'react-router-dom';
 
 const navbar = props => (
   <nav className={`navbar ${props.text}`}>
+    <h1 className="logo">
+      <NavLink activeClassName="active" to="/">
+        RAUL
+      </NavLink>
+    </h1>
     <ul className="menu">
       <li className="menu-item">
-        <NavLink to="/about">ABOUT</NavLink>
+        <NavLink activeClassName="active" to="/about">
+          ABOUT
+        </NavLink>
       </li>
       <li className="menu-item">
-        <NavLink to="/work">WORK</NavLink>
+        <NavLink activeClassName="active" to="/work">
+          WORK
+        </NavLink>
       </li>
       <li className="menu-item">
-        <NavLink to="/contact">CONTACT</NavLink>
+        <NavLink activeClassName="active" to="/contact">
+          CONTACT
+        </NavLink>
       </li>
     </ul>
   </nav>
